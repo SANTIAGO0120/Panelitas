97cab3a76d4dadc4c7a2d61b2d13233797fe6f45
SANTIAGO0120 <u20201187651@usco.edu.co>
2023-11-14 19:09:04 -0500

    Se agregar mi proyecto

---
+++ demo/demo/src/main/java/com/example/demo/Java/LoginForm.java
@@ -0,0 +1,24 @@
+package com.example.demo.Java;
+
+public class LoginForm {
+    private String username;
+    private String password;
+
+    // Getters
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    // Setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ demo/demo/src/main/java/com/example/demo/Java/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.Java;
+
+public class UserRepository {
+
+    public void save(User user) {
+    }
+
+}
